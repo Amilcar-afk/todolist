0b13ee271401fe1dd7fd5d37337bae7108ce2753
Amilcar <fernandesamilcar28@gmail.com>
2024-10-15 21:52:51 +0200

    addUsers client part ok

--- front/src/App.js
+++ front/src/App.js
@@ -3,16 +3,17 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import 'flowbite/dist/flowbite.css';
 import RegisterPage from "./features/Auth/RegisterPage";
+import AuthProvider from "./contexts/AuthContext";
 
 
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
+  /*useEffect(() => {
     fetch('http://localhost:8000/api/hello')
         .then(response => response.text())
         .then(message => setMessage(message));
-  }, []);
+  }, []);*/
 
   useEffect(() => {
     console.log(message);
@@ -20,9 +21,11 @@ function App() {
 
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/register" element={<RegisterPage/>} />
-        </Routes>
+          <AuthProvider>
+            <Routes>
+              <Route path="/register" element={<RegisterPage/>} />
+            </Routes>
+          </AuthProvider>
       </BrowserRouter>
   );
 }
---
+++ front/src/services/axiosRequestFunction.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.SYMFONY_APP_SERVER;
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8000",
+});
+
+const sendRequest = async (endpoint, method = 'GET', data = {}, requireAuth = true, params = {}, contentType = 'application/json') => {
+    try {
+        return await axiosInstance({
+            url: endpoint,
+            method,
+            data,
+            params,
+            headers: {
+                'Content-Type': contentType,
+                'Accept': '*/*'
+            },
+        })
+    } catch (error) {
+        //console.error('Error with the request:', error.response?.data || error.message);
+        throw error;
+    }
+};
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('jwtToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default sendRequest;
