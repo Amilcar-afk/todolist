9bb004a55b65fdb205070b0a219e536a9f2c97c7
Amilcar <fernandesamilcar28@gmail.com>
2024-10-17 21:31:27 +0200

    burger menu front ok

--- front/src/features/ToDoListDashboard/components/SideMenu.js
+++ front/src/features/ToDoListDashboard/components/SideMenu.js
@@ -1,16 +1,13 @@
-import { Button, Drawer, Sidebar, TextInput } from "flowbite-react";
+import { Button, Drawer, Sidebar, TextInput} from "flowbite-react";
 import { useState } from "react";
 import {
-    HiChartPie,
-    HiClipboard,
-    HiCollection,
     HiInformationCircle,
     HiLogin,
-    HiPencil,
+    HiPlusCircle,
     HiSearch,
-    HiShoppingBag,
-    HiUsers,
+    HiTag
 } from "react-icons/hi";
+import {HiCalendarDays, HiCog6Tooth, HiRectangleStack} from "react-icons/hi2";
 
 export function SideMenu() {
     const [isOpen, setIsOpen] = useState(true);
@@ -19,11 +16,16 @@ export function SideMenu() {
 
     return (
         <>
-            <div className="flex min-h-[50vh] items-center justify-center">
-                <Button onClick={() => setIsOpen(true)}>Show navigation</Button>
+            <div className="relative min-h-[50vh]">
+                <Button
+                    onClick={() => setIsOpen(true)}
+                    className="absolute top-0 left-0 m-4 bg-blue-700 hover:bg-blue-800 text-white font-medium rounded-lg text-sm px-4 text-center"
+                >
+                    Show navigation
+                </Button>
             </div>
             <Drawer open={isOpen} onClose={handleClose}>
-                <Drawer.Header title="MENU" titleIcon={() => <></>} />
+                <Drawer.Header title="MENU" titleIcon={() => <></>}/>
                 <Drawer.Items>
                     <Sidebar
                         aria-label="Sidebar with multi-level dropdown example"
@@ -36,31 +38,30 @@ export function SideMenu() {
                                 </form>
                                 <Sidebar.Items>
                                     <Sidebar.ItemGroup>
-                                        <Sidebar.Item href="/" icon={HiChartPie}>
-                                            Dashboard
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="/e-commerce/products" icon={HiShoppingBag}>
-                                            Products
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="/users/list" icon={HiUsers}>
-                                            Users list
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="/authentication/sign-in" icon={HiLogin}>
-                                            Sign in
-                                        </Sidebar.Item>
-                                        <Sidebar.Item href="/authentication/sign-up" icon={HiPencil}>
-                                            Sign up
+                                        <Sidebar.Item className="navbar-item-hover text-[#ff8d50]" href="#" icon={() => <HiPlusCircle className="text-[#ff8d50] w-6 h-6" />}>
+                                            Ajouter une tâche
                                         </Sidebar.Item>
                                     </Sidebar.ItemGroup>
                                     <Sidebar.ItemGroup>
-                                        <Sidebar.Item href="https://github.com/themesberg/flowbite-react/" icon={HiClipboard}>
-                                            Docs
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiRectangleStack className="text-[#ff8d50] w-6 h-6" />}>
+                                            Aujourd'hui
                                         </Sidebar.Item>
-                                        <Sidebar.Item href="https://flowbite-react.com/" icon={HiCollection}>
-                                            Components
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiCalendarDays className="text-[#ff8d50] w-6 h-6" />}>
+                                            Calendrier
                                         </Sidebar.Item>
-                                        <Sidebar.Item href="https://github.com/themesberg/flowbite-react/issues" icon={HiInformationCircle}>
-                                            Help
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiTag className="text-[#ff8d50] w-6 h-6" />} >
+                                            Catégories et Etiquettes
+                                        </Sidebar.Item>
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiCog6Tooth className="text-[#ff8d50] w-6 h-6" />}>
+                                            Mes informations
+                                        </Sidebar.Item>
+                                    </Sidebar.ItemGroup>
+                                    <Sidebar.ItemGroup>
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiInformationCircle className="text-[#ff8d50] w-6 h-6" />}>
+                                            Aide
+                                        </Sidebar.Item>
+                                        <Sidebar.Item className="navbar-item-hover" href="#" icon={() => <HiLogin className="text-[#ff8d50] w-6 h-6" />}>
+                                            Se déconnecter
                                         </Sidebar.Item>
                                     </Sidebar.ItemGroup>
                                 </Sidebar.Items>
