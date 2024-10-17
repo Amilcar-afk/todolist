6c97c043a8937156f3e414609429d0abab7b03c2
Amilcar <fernandesamilcar28@gmail.com>
2024-10-17 19:31:25 +0200

    header component done

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-rou
 import 'flowbite/dist/flowbite.css';
 import RegisterPage from "./features/Auth/RegisterPage";
 import AuthProvider from "./contexts/AuthContext";
+import LoginPage from "./features/Auth/LoginPage";
+import ToDoListDashboardPage from "./features/ToDoListDashboard/ToDoListDashboardPage";
 
 
 function App() {
@@ -23,7 +25,9 @@ function App() {
       <BrowserRouter>
           <AuthProvider>
             <Routes>
-              <Route path="/register" element={<RegisterPage/>} />
+                <Route path="/register" element={<RegisterPage/>} />
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/todolist" element={<ToDoListDashboardPage/>} />
             </Routes>
           </AuthProvider>
       </BrowserRouter>
