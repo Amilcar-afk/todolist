cc0ec2fa509a1adf071aff7dffb9c7f960ccaa23
Amilcar <fernandesamilcar28@gmail.com>
2024-11-28 19:15:37 +0100

    add loading to register and login

--- front/src/features/Auth/components/RegisterForm.jsx
+++ front/src/features/Auth/components/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+import { DarkThemeToggle, Flowbite, Spinner } from 'flowbite-react';
 import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../../../contexts/AuthContext.jsx";
 
@@ -13,6 +13,7 @@ const RegisterForm = () => {
         email: '',
         password: '',
     });
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -24,6 +25,7 @@ const RegisterForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsSubmitting(true);
         const formattedData = {
             'firstname': userData.firstname,
             'lastname': userData.lastname,
@@ -33,11 +35,17 @@ const RegisterForm = () => {
         const responseCode = await register(formattedData);
         if (responseCode === 201){
             navigate('/login');
+        }else{
+            setIsSubmitting(false);
         }
     };
 
     return (
         <Flowbite>
+            {isSubmitting && (
+                <div className="spinner-div fixed inset-0 z-2 bg-black bg-opacity-50 flex items-center justify-center">
+                </div>
+            )}
             <div
                 className=" container-form min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
                 <h1 className="text-4xl font-bold mb-6">Inscription</h1>
@@ -94,7 +102,11 @@ const RegisterForm = () => {
                         type="submit"
                         className="w-full button-primary-color hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none"
                     >
-                        S'inscrire
+                        {isSubmitting ? (
+                            <Spinner size="sm" aria-label="Chargement..." />
+                        ) : (
+                            "S'inscrire"
+                        )}
                     </button>
                 </form>
 
