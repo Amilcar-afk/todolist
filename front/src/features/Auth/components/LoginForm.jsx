5aa4a348fca0d98116f0a33308425980cda689d8
Amilcar <fernandesamilcar28@gmail.com>
2024-11-28 19:15:37 +0100

    add loading to register and login

--- front/src/features/Auth/components/LoginForm.jsx
+++ front/src/features/Auth/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useState} from 'react';
-import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+import { DarkThemeToggle, Flowbite, Spinner } from 'flowbite-react';
 import {useNavigate} from "react-router-dom";
 import {AuthContext} from "../../../contexts/AuthContext.jsx";
 
@@ -11,6 +11,7 @@ const LoginForm = () => {
         email: '',
         password: '',
     });
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -22,6 +23,7 @@ const LoginForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsSubmitting(true);
         const formattedData = {
             'email': userData.email,
             'password': userData.password
@@ -29,11 +31,17 @@ const LoginForm = () => {
         const responseCode = await login(formattedData);
         if (responseCode === 200){
             navigate('/todolist');
+        }else{
+            setIsSubmitting(false);
         }
     };
 
     return (
         <Flowbite>
+            {isSubmitting && (
+                <div className="spinner-div fixed inset-0 z-2 bg-black bg-opacity-50 flex items-center justify-center">
+                </div>
+            )}
             <div
                 className=" container-form min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
                 <h1 className="text-4xl font-bold mb-6">Connexion</h1>
@@ -67,7 +75,11 @@ const LoginForm = () => {
                         type="submit"
                         className="w-full button-primary-color hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none"
                     >
-                        Se connecter
+                        {isSubmitting ? (
+                            <Spinner size="sm" aria-label="Chargement..." />
+                        ) : (
+                            "Se connecter"
+                        )}
                     </button>
                 </form>
 
