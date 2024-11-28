0161b47cabed1e6ff37535979485226711baa956
Amilcar <fernandesamilcar28@gmail.com>
2024-11-28 20:56:48 +0100

    add future task table display

--- front/src/features/ToDoListDashboard/ToDoListDashboardPage.jsx
+++ front/src/features/ToDoListDashboard/ToDoListDashboardPage.jsx
@@ -6,24 +6,29 @@ import { TodayTaskTable } from "./components/Table/TodayTaskTable";
 import './styles/ToDoListDashboard.css';
 import {TaskContext} from "../../contexts/TaskContext";
 import {OverdueTodayTaskTable} from "./components/Table/OverdueTaskTable";
+import { FutureTaskTable } from './components/Table/FutureTaskTable.jsx';
 
 const ToDoListDashboardPage = () => {
     const [openModal, setOpenModal] = useState(false);
-    const { displayTodayTask, displayOverdueTask } = useContext(TaskContext);
+    const { displayTodayTask, displayOverdueTask, displayFutureTask } = useContext(TaskContext);
     const currentDate = new Date();
     const formattedDate = currentDate.toISOString().split('T')[0];
 
     useEffect(() => {
         displayTodayTask(formattedDate);
         displayOverdueTask(formattedDate);
+        displayFutureTask(formattedDate);
     }, []);
 
     return (
         <>
             <Header />
             <div className="main-content">
-                <SideMenu setOpenModal={setOpenModal} />
-                <TodayTaskTable />
+            <SideMenu setOpenModal={setOpenModal} />
+                <div className="div-row">
+                    <TodayTaskTable />
+                    <FutureTaskTable />
+                </div>
                 <OverdueTodayTaskTable/>
             </div>
             <NewTaskFormModal openModal={openModal} setOpenModal={setOpenModal}/>
