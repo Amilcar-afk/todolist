f4d277d2a07779599ce5a5361e9a7cbcc65875a0
Amilcar <fernandesamilcar28@gmail.com>
2025-01-02 19:17:48 +0100

    fix menu button appearance and display

--- front/src/features/CalendarView/CalendarViewPage.jsx
+++ front/src/features/CalendarView/CalendarViewPage.jsx
@@ -2,9 +2,11 @@ import React, {useContext, useEffect, useState} from 'react';
 import Calendar from "./components/Calendar.jsx";
 import {TaskContext} from "../../contexts/TaskContext";
 import { Header } from '../Header/Header.jsx';
+import { SideMenu } from "../SideBar/SideMenu.jsx";
 
 const CalendarViewPage = () => {
     const { getUserTasks, UserTasks } = useContext(TaskContext);
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         getUserTasks();
@@ -13,7 +15,10 @@ const CalendarViewPage = () => {
     return (
         <>
             <Header/>
-            <Calendar/>
+            <div>
+                <SideMenu setOpenModal={setOpenModal} />
+                <Calendar/>
+            </div>
         </>
     );
 };
